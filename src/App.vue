7950d36d517c032b82245085b82b8d4198f3886d
Daniel Hernández <danhernan23i@gmail.com>
2021-03-30 01:50:24 -0400

    Add initial homepage

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
-<template>
-  <router-view />
-</template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import ParticlesBg from 'particles-bg-vue/src/particles-bg/ParticlesBg.vue';
+import Navigation from '@/components/Navigation.vue';
+
 export default defineComponent({
   name: 'App',
+  components: {
+    ParticlesBg,
+    Navigation,
+  },
 });
 </script>
 
+<template>
+  <div class="App h-screen w-screen overflow-y-hidden">
+    <ParticlesBg
+      class="absolute z-0 blur"
+      type="circle"
+      :color="['#F6453A']"
+      :num="7"
+    />
+    <div
+      class="absolute z-10 h-full w-full bg-background bg-opacity-80 overflow-y-auto"
+    >
+      <Navigation />
+      <router-view />
+    </div>
+  </div>
+</template>
+
 <style>
+:root {
+  background-color: #212121;
+}
+
+.blur {
+  filter: blur(64px);
+  -webkit-filter: blur(64px);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: white;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavigationBar',
+  setup() {
+    const Links = [
+      {
+        title: 'About',
+        url: '/about',
+      },
+    ];
+
+    return {
+      Links,
+    };
+  },
+});
+</script>
+
+<template>
+  <div
+    class="flex flex-row justify-between items-center px-4 py-6 font-bold tracking-widest text-lg"
+  >
+    <router-link to="/" class="flex">
+      <p>Logo</p>
+      SongTube
+    </router-link>
+    <div class="flex">
+      <router-link
+        v-for="link in Links"
+        :key="`link-${link.title}`"
+        :to="link.url"
+      >
+        {{ link.title }}
+      </router-link>
+    </div>
+  </div>
+</template>
