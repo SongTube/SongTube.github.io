da89a4233dcfbffa169a10be2231d169717c63ee
Daniel Hernández <danhernan23i@gmail.com>
2021-03-29 21:46:49 -0400

    Add brand colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#F6453A',
+        background: '#212121',
+      },
+    },
   },
   variants: {
     extend: {},
