cfad0f765fb6c9ca8a3bf89aca26d833b36a8f00
Daniel Hernández <danhernan23i@gmail.com>
2021-03-31 16:33:39 -0400

    Remove about page
    for now ig

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -26,14 +26,5 @@ export default defineComponent({
       <img class="h-14 w-14 mr-2" src="@/assets/images/logo.png" />
       SongTube
     </router-link>
-    <div class="flex mr-4 text-lg">
-      <router-link
-        v-for="link in Links"
-        :key="`link-${link.title}`"
-        :to="link.url"
-      >
-        {{ link.title }}
-      </router-link>
-    </div>
   </div>
 </template>
